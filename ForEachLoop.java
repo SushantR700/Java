cf55154de7ffbfe3288a9d4f80b065477f67b65b
Sushant <sushantregmi655@gmail.com>
2023-03-23 17:53:14 +0545

    For Each

---
+++ ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static void main(String[] args){
+        String[] cars={"BMW","Buggati","Toyota"};
+        for(String car:cars){
+            System.out.println(car);
+
+        }
+    }
+}
